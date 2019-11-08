06e25cb8ac273120278e043dcc06b3a93252d04e
Vidip Ganesh Goriparthi <vidipganesh@gmail.com>
2019-11-08 17:09:38 -0500

    added comments

--- src/main/java/FactGeneration.java
+++ src/main/java/FactGeneration.java
@@ -6,28 +6,38 @@ public class FactGeneration {
 
   public static void getFacts() {
     try {
+      //URL of the API from where random facts are generates.
       URL url = new URL("https://uselessfacts.jsph.pl/random.txt?language=en");
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
+
+      //Initializes a connection to the URL.
+      HttpURLConnection APIConnection = (HttpURLConnection) url.openConnection();
+
+      //Type of HTTPRequest being made to the URL.
+      APIConnection.setRequestMethod("GET");
       BufferedReader in = new BufferedReader(
-          new InputStreamReader(con.getInputStream()));
+          new InputStreamReader(APIConnection.getInputStream()));
       String inputLine;
       StringBuilder content = new StringBuilder();
 
+      //Builds the string that is tweeted.
       while ((inputLine = in.readLine()) != null) {
         content.append(inputLine);
       }
+
+      //Cleans up the formatting of the string to be tweeted.
       content.replace(0, 2, "");
 
-      System.out.println(content);
-      String fileName = "/Users/Vidip/Everything/CS/Java/Intellij/TwitterBot1/src/main/resources/tweets.txt";
-      BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+      //Creates a BufferedWriter with the source at the filepath- fileName.
+      BufferedWriter writer = new BufferedWriter(
+          new FileWriter(TwitterAutoBot.fileName, true));
       PrintWriter out = new PrintWriter(writer);
+
+      //Writes the string to the File.
       out.write(content.toString());
       out.write('\n');
       out.close();
       in.close();
-      con.disconnect();
+      APIConnection.disconnect();
 
     }
     catch (Exception e) {
--- src/main/java/TwitterAutoBot.java
+++ src/main/java/TwitterAutoBot.java
@@ -2,39 +2,54 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.*;
 import java.nio.charset.Charset;
 
 public class TwitterAutoBot {
 
+  //Path to file that contains tweets
+  public static String fileName =
+      "/Users/Vidip/Everything/CS/Java/Intellij/TwitterBot1/src/main/resources/tweets.txt";
+
+  //Calls static method tweetLines().
   public static void main(String[] args) {
     tweetLines();
   }
 
+  //Reads a locally saved text file, and tweets them.
   private static void tweetLines() {
+
+    //Text that is going to be tweeted.
     String tweet;
+
+    //Count of tweets tweeted in this session.
     int count = 0;
+
     while (count < 10) {
       try {
-        try (InputStream fis = new FileInputStream(
-            "/Users/Vidip/Everything/CS/Java/Intellij/TwitterBot1/src/main/resources/tweets.txt");
+        try (InputStream fis = new FileInputStream(fileName);
             InputStreamReader isr = new InputStreamReader(fis, Charset.forName("Cp1252"));
-            BufferedReader br = new BufferedReader(isr);) {
+            BufferedReader br = new BufferedReader(isr)) {
+
+          //Calls static method GetFacts from the FactGeneration class.
           FactGeneration.getFacts();
-          tweet=br.readLine();
+          tweet = br.readLine();
           while (tweet != null) {
+
+            //sendTweet accesses the TwitterAPI and posts the tweet.
             sendTweet(tweet);
-            System.out.println(tweet);
+            System.out.println("tweeting:" + tweet);
             try {
-              int sleepTime = 18000;
+              //Pauses the thread for the given amount of time.
+              int sleepTime = 18000; //in milliseconds.
               System.out.printf("Sleeping for %d seconds", sleepTime / 1000);
               Thread.sleep(sleepTime);
             }
             catch (InterruptedException e) {
               e.printStackTrace();
             }
+            //moves to the next line in the file.
             tweet = br.readLine();
           }
         }
@@ -46,6 +61,7 @@ public class TwitterAutoBot {
     }
   }
 
+  //Calls the TwitterAPI and posts the given string as a tweet.
   private static void sendTweet(String line) {
     Twitter twitter = TwitterFactory.getSingleton();
     Status status;
